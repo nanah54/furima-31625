b140a63abbcac3486acd7eada0cdcbafd9a291ab
nanah54 <dna.wspiral.acgt@mopera.net>
2020-11-19 14:42:59 +0900

    モデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   validates :first_name_pronunciation_key, presence: true 
   validates :birthday, presence: true 
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :family_name_pronunciation_key
+    validates :first_name_pronunciation_key
+  end
+
   has_many :items
   has_many :buyers
 end
