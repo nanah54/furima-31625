e7f1d84b855e525f68b0d98b2c6c8c0249a00976
nanah54 <dna.wspiral.acgt@mopera.net>
2020-11-21 19:21:22 +0900

    出品機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:title, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :price, :category_id, :usage_status_id,:postage_id, :shipment_source_id, :date_of_shipment_id, :concept, :user, ).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,11 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :shipment_source
   belongs_to :date_of_shipment
-  validates :title, :text, presence: true
+  validates :title,  presence: true
   validates :category_id , numericality: { other_than: 1 } 
   validates :usage_status_id, numericality: { other_than: 1 } 
   validates :postage_id, numericality: { other_than: 1 } 
   validates :shipment_source_id, numericality: { other_than: 1 } 
   validates :date_of_shipment_id, numericality: { other_than: 1 } 
+  validates :price,  presence: true
 end
