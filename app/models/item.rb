cb47ee9420f37e013106e48098ea6b9ac817b26d
nanah54 <dna.wspiral.acgt@mopera.net>
2020-11-23 12:36:42 +0900

    モデルの単体テストコードと画像の制限を書く

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,4 +17,10 @@ class Item < ApplicationRecord
   validates :shipment_source_id, numericality: { other_than: 1 } 
   validates :date_of_shipment_id, numericality: { other_than: 1 } 
   validates :price,  presence: true
+  validates :image, presence: true
+  validates :concept,  presence: true
+  with_options presence: true, numericality: { with: /\A^[0-9]+$\z/, message: '半角数字を使用してください' } do
+    validates :price
+  end
+  validates_inclusion_of :price, in: 300..9999999, message: '値段は300~9.999.999の間でお願いします'
 end
