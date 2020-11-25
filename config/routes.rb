e872b99ef6e230198349339d587eec5204365fe6
nanah54 <dna.wspiral.acgt@mopera.net>
2020-11-25 16:18:56 +0900

    再修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new, :create, :show, :destroy,:edit,:update]
+  resources :items do
   resources :buyers, only: [:index,:create]
 end
