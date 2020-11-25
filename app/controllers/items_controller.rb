79c695f60ec8ca520f0658a1e530852f5f7514fc
nanah54 <dna.wspiral.acgt@mopera.net>
2020-11-25 17:39:14 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,8 +12,7 @@ class ItemsController < ApplicationController
 
   def update
     
-    @item.update(item_params)
-    if  @item.save
+    if  @item.update(item_params)
      redirect_to root_path
     else
       render :edit
