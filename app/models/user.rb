a8828961217d72a668f88c330ce898dc385e82b3
nanah54 <dna.wspiral.acgt@mopera.net>
2020-12-02 19:51:22 +0900

    パスワード制限と詳細表示の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
     validates :birthday
   end
 
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字混合にしてください' } do
+  validates :encrypted_password
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
